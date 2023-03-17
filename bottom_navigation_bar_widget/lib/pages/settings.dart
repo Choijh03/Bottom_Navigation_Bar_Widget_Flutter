0639731984b4ae19f3501bceeeec075292c67ff3
Jihun Choi <98497929+Choijh03@users.noreply.github.com>
2023-03-16 22:45:29 -0700

    code updated
    code updated

---
+++ bottom_navigation_bar_widget/lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserSettings extends StatelessWidget {
+  const UserSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Setting page',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
